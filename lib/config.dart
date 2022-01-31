50c674e93f9434342dc98fd497a3b3659f191925
Shahadat Hossain <76393342+AxelTahmid@users.noreply.github.com>
2022-01-31 17:55:54 +0600

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class Config {
   //WeDeliververFlutter
-  static String key = "ck_e26a777da3190f037e36691ddd8994dcc8f0a2bd";
-  static String secret = "cs_6f6f47948c59f64665469345a2103235e14a300c";
+  static String key = "";
+  static String secret = "";
 
   static String url = "https://wedeliver-bd.com//wp-json/wc/v3/";
   static String customerURL = "customers";
